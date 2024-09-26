b9fc11e9df70eb5a3b8ac8426eac446a734773dd
luckys-lnz <lalucky22@gmail.com>
2024-09-26 11:23:04 +0000

    modified some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'newApp';
+  title = 'k12';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { TrackComponent } from './track/track.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home Page',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    title: 'About Us',
+    component: AboutComponent,
+  },
+  {
+    path: 'track',
+    title: 'Track Package',
+    component: TrackComponent,
+  },
+  {
+    path: 'services',
+    title: 'Our Services',
+    component: ServicesComponent,
+  },
+  {
+    path: 'contact',
+    title: 'Contact Us',
+    component: ContactComponent,
+  },
+];
